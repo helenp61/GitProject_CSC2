522d75cd059aed52adabf604c27370b1577781d6
Helen Prasad <HPrasad@ormiston.school.nz>
2023-04-28 06:51:35 +1200

    Added Frame to remove white spaces off buttons

--- background_img.py
+++ background_img.py
@@ -16,8 +16,19 @@ my_label.place(x=0, y=0, relwidth=1, relheight=1)
 my_text = Label(root, text="Welcome!", font=("Helvetica", 50), fg="black")
 my_text.pack(pady=50)
 
-# Add some buttons
-my_button1 = Button(root, text="Exit")
+# Create a frame - for background to stand out
+my_frame = Frame(root)
+my_frame.pack(pady=20)
 
+
+# Add some buttons and now put buttons inside the frame just added
+my_button1 = Button(my_frame, text="Exit")
+my_button1.grid(row=0, column=0, padx=20)
+
+my_button2 = Button(my_frame, text="Start")
+my_button2.grid(row=0, column=1, padx=20)
+
+my_button3 = Button(my_frame, text="Reset")
+my_button3.grid(row=0, column=2, padx=20)
 
 root.mainloop()
