31db5d4afaa538b86ba447e1534a1ddd5ebdb694
Helen Prasad <HPrasad@ormiston.school.nz>
2023-04-27 12:22:45 +1200

    First Commit

---
+++ Copy of 1_ex_1.py
@@ -0,0 +1,25 @@
+from tkinter import *  # imports everything from TK - tkinter is a module
+from tkinter import ttk  # ttk is a sub module of tkinter
+
+root = Tk()  # top level window
+
+# Create a Label Widget
+label = Label(root, text="Hello Python")  # what you see
+# on screen
+
+# font colour, config is for properties
+label.config(text="Hello Python",borderwidth=1,relief="solid",font="Times 32, fg="red")
+label.config(bg="yellow")  # background colour
+#label.config(font='Times 20')
+
+label.config(text='Python is a great program and we can do lots with it')
+label.config(wraplength='150')  # wrap text if long label
+label.config(justify="right")
+
+# Showing it on the screen
+label.pack()
+root.geometry("300x250")
+
+root.mainloop()  # GUI is always looping -
+# when you run your mouse over and you can click on
+# #any buttons/labels
---
+++ Copy of 2_button_1.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+root.geometry("500x400")
+# some widgets available only in ttk
+# and some in Tkinter
+# we will see the difference here
+# with the buttons
+
+
+button1 = Button(root, text="Click Me!")  # button created using tkinter
+button2 = ttk.Button(root, text="Click Me!")  # button created using ttk
+# if you run it now you will see an empty GUI -
+# thebuttons do not show up
+# have to use geometry manager to be able
+# to see the buttons
+# here we will use .pack to show our buttons
+
+button1.pack()
+button2.pack()
+
+"""we can also define the size of the window
+i.e. the x and y value for our window 
+this will be added just below your root which 
+is above"""
+root.mainloop()
